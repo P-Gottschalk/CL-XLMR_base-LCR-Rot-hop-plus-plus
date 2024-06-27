d89f4d2f59949f732dc7bb8dcc440fb25bf6447f
Philipp <164037991+SmartStevie02@users.noreply.github.com>
2024-06-27 17:45:34 +0200

    Add files via upload

---
+++ model/embeddings_layer_xlmr.py
@@ -0,0 +1,55 @@
+from typing import Optional
+
+import torch
+from transformers import XLMRobertaTokenizer, XLMRobertaModel
+
+print()
+
+class EmbeddingsLayerXLMR:
+
+    """
+    str training determines which RoBERTa base you use
+    Opt. 1: "xlm-roberta-base"
+    Opt. 2: "xlm-roberta-large"
+    """
+
+    def __init__(self, training: str, device = torch.device('cpu')):
+        super().__init__()
+        
+        self.device = device
+        self.tokenizer: XLMRobertaTokenizer = XLMRobertaTokenizer.from_pretrained(training)
+        self.model: XLMRobertaModel = XLMRobertaModel.from_pretrained(training, output_hidden_states = True)
+        self.model.eval()
+    
+    def forward(self, sentence: str, target_start: int, target_end: int) -> tuple[
+        torch.Tensor, tuple[int, int], Optional[torch.Tensor]
+    ]:
+        sentence = f"[CLS] {sentence} [SEP]"
+        target_start += 6
+        target_end += 6
+
+
+        left_str = self.tokenizer.tokenize(sentence[0:target_start])
+        target_str = self.tokenizer.tokenize(sentence[target_start:target_end])
+        target_index_start = len(left_str) - 1
+        target_index_end = target_index_start + len(target_str)
+
+        tokens = self.tokenizer(sentence, return_tensors="pt")
+        
+        with torch.no_grad():
+            results = self.model(**tokens)
+
+        last_hidden_states = results.hidden_states
+        
+        last_layers = last_hidden_states[-4:]
+        unrounded_embeddings = torch.stack(last_layers).sum(dim=0) / 4
+
+        n_digits = 8
+        embeddings = torch.round(unrounded_embeddings * 10 ** n_digits) / (10 ** n_digits)
+
+        embeddings = embeddings[0][1:-1]
+        # print(embeddings) -> debugging
+
+        return embeddings, (target_index_start, target_index_end), None
+
+
---
+++ utils/sent_con_loss.py
@@ -0,0 +1,79 @@
+# Adapted from https://github.com/GKLMIP/CL-XABSA.git
+import numpy as np
+import torch
+
+class SentConLoss(torch.nn.Module):
+
+    def __init__(self, temperature = 0.07, base_temperature = 0.07):
+        super(SentConLoss, self).__init__()
+        self.temperature = temperature
+        self.base_temperature = base_temperature
+    
+    def forward(self, batch, labels): 
+
+        device = (torch.device('cuda') if batch.is_cuda else torch.device('cpu'))
+
+        batch_size = batch.shape[0]
+
+        #Creates a label matrix
+        labels = labels.contiguous().view(-1,1)
+        labels_ = []
+        for i in labels.tolist():
+            # print(i[0]) -> debugging
+            if i[0] == 0:
+                labels_.append([1])
+            elif i[0] == 1:
+                labels_.append([2])
+            elif i[0] == 2:
+                labels_.append([3])
+                
+        labels_ = torch.Tensor(labels_)
+
+        if labels_.shape[0] != batch_size:
+            raise ValueError('Num of labels does not match the size of the batch')
+        mask = torch.eq(labels_, labels_.T).float().to(device)
+
+        contrast_count = batch.shape[1]
+        contrast_feature = torch.cat(torch.unbind(batch, dim=1), dim=0).to(device)
+
+        # print("Contrast feature", contrast_feature)
+
+        anchor_feature = contrast_feature
+        anchor_count = contrast_count
+
+        # compute logits
+        anchor_dot_contrast = torch.div(torch.matmul(anchor_feature, contrast_feature.T), self.temperature)
+
+        # print("Shape of anchor_dot_contrast:", anchor_dot_contrast.shape)
+
+        # for numerical stability  
+        logits_max, _ = torch.max(anchor_dot_contrast, dim=0, keepdim=True)
+        logits = anchor_dot_contrast - logits_max.detach()
+
+        # tile mask
+        mask = mask.repeat(anchor_count, contrast_count)
+        # mask-out self-contrast cases
+        logits_mask = torch.scatter(
+            torch.ones_like(mask),
+            1,
+            torch.arange(batch_size * anchor_count).view(-1, 1).to(device),
+            0
+        )
+        mask = mask * logits_mask
+
+
+        # compute log_prob
+        exp_logits = torch.exp(logits) * logits_mask + 1e-20
+        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
+
+        # compute mean of log-likelihood over positive
+        mean_log_prob_pos = ((mask * log_prob).sum(1) + 1e-20) / (mask.sum(1) + 1e-20)
+
+        # loss
+        loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
+        loss = loss.view(anchor_count, batch_size).mean()
+
+        torch.cuda.empty_cache()
+        del mask, logits_mask, logits, exp_logits, log_prob, mean_log_prob_pos, anchor_dot_contrast, logits_max
+
+        return loss
